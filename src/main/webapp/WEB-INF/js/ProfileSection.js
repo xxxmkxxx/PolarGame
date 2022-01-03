4dd4b8031fe4e476b6b0a195f8022384f9df57f9
Tatiana-Popova <Tatiana-Popova@users.noreply.github.com>
2022-01-03 15:12:32 +0400

    Добавила js функции в разделе профиля

---
+++ src/main/webapp/WEB-INF/js/ProfileSection.js
@@ -0,0 +1,52 @@
+const chooseSection = () => {
+  $(document).on('click', '#settings_button', function () {
+    $('#friends_section').css('display', 'none');
+    $('#settings_section').css('display', 'flex');
+    $('#settings_button').css('opacity', '100%');
+    $('#friends_button').css('opacity', '50%');
+  });
+
+  $(document).on('click', '#friends_button', function () {
+    $('#settings_section').css('display', 'none');
+    $('#friends_section').css('display', 'flex');
+    $('#friends_button').css('opacity', '100%');
+    $('#settings_button').css('opacity', '50%');
+  });
+};
+
+const descriptionEvent = () => {
+  let doesInEditMode = false;
+
+  $(document).on('click', '#edit_description', function () {
+    if (doesInEditMode === false) {
+      document.getElementById('edit_description').src = '../images/apply.png';
+      $('#user_description_area').css('pointer-events', 'auto');
+      doesInEditMode = true;
+    } else {
+      document.getElementById('edit_description').src = '../images/edit_icon.png';
+      $('#user_description_area').css('pointer-events', 'none');
+      doesInEditMode = false;
+    }
+  });
+};
+
+const deleteConfirmation = () => {
+  $(document).on('click', '#delete_friend', function () {
+    $('#whiteBack').css('display', 'flex');
+    $('#confirmationBlock').css('display', 'flex');
+  });
+
+  $(document).on('click', '#confirmationToDelete', function () {
+    $('#whiteBack').css('display', 'none');
+    $('#confirmationBlock').css('display', 'none');
+  });
+
+  $(document).on('click', '#confirmationCancel', function () {
+    $('#whiteBack').css('display', 'none');
+    $('#confirmationBlock').css('display', 'none');
+  });
+};
+
+chooseSection();
+descriptionEvent();
+deleteConfirmation();
