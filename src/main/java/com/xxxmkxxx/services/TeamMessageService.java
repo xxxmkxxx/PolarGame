fb16c7dbe5d1e02e3ec250b35639f4723f3bb85c
xxxmkxxx <maks88648@gmail.com>
2022-01-30 15:30:12 +0400

    Рефакторинг сервиса TeamMessageService.

--- src/main/java/com/xxxmkxxx/services/TeamMessageService.java
+++ src/main/java/com/xxxmkxxx/services/TeamMessageService.java
@@ -1,38 +1,26 @@
 package com.xxxmkxxx.services;
 
-import com.xxxmkxxx.dao.TeamDAO;
-import com.xxxmkxxx.dao.TeamMessageDAO;
+import com.xxxmkxxx.dao.TeamMessageDAOImpl;
 import com.xxxmkxxx.models.TeamMessageModel;
 import com.xxxmkxxx.models.TeamModel;
 import com.xxxmkxxx.models.UserModel;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 @Component
 public class TeamMessageService {
-    private TeamDAO teamDAO;
-    private TeamMessageDAO teamMessageDAO;
-
-    @Transactional
-    public List<TeamMessageModel> getMessages(TeamModel team) {
-        return teamDAO.initializeMessages(team).getMessages();
-    }
+    private TeamMessageDAOImpl dao;
 
     @Transactional
     public TeamMessageModel createTeamMessage(String text, UserModel user, TeamModel team) {
         TeamMessageModel message = new TeamMessageModel(text, user, team);
 
-        teamMessageDAO.saveMessage(message);
-        teamDAO.updateTeam(team);
+        dao.create(message);
 
         return message;
     }
 
-    public TeamMessageService(TeamDAO teamDAO, TeamMessageDAO teamMessageDAO) {
-        this.teamDAO = teamDAO;
-        this.teamMessageDAO = teamMessageDAO;
+    public TeamMessageService(TeamMessageDAOImpl dao) {
+        this.dao = dao;
     }
 }
