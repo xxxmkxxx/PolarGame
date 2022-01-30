512b5d2cbb852bb986ecac0cff274d26dae3e6b3
xxxmkxxx <maks88648@gmail.com>
2022-01-30 16:03:25 +0400

    Переименовал класс PartyMemberDAO в PartyMemberDAOImpl и отрефакторил его.

--- src/main/java/com/xxxmkxxx/dao/PartyMemberDAOImpl.java
+++ src/main/java/com/xxxmkxxx/dao/PartyMemberDAOImpl.java
@@ -2,23 +2,21 @@ package com.xxxmkxxx.dao;
 
 import com.xxxmkxxx.models.PartyMemberModel;
 import com.xxxmkxxx.models.PartyModel;
-import com.xxxmkxxx.models.UserModel;
 import org.hibernate.Hibernate;
 import org.hibernate.LockMode;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 @Component
-public class PartyMemberDAO {
+public class PartyMemberDAOImpl extends AbstractDAO<PartyMemberModel> implements PartyMemberDAO {
     private SessionFactory sessionFactory;
 
-    public void savePartyMember(PartyMemberModel partyMember) {
-        Session session = sessionFactory.getCurrentSession();
-        session.save(partyMember);
+    public PartyMemberDAOImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
     }
 
+    @Override
     public PartyModel initPartyMembers(PartyModel party) {
         Session session = sessionFactory.getCurrentSession();
         session.lock(party, LockMode.NONE);
@@ -26,9 +24,5 @@ public class PartyMemberDAO {
         Hibernate.initialize(party.getMembers());
 
         return party;
-    }
-
-    public PartyMemberDAO(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
     }
 }
