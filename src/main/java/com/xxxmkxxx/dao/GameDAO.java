d25e647b01286d1428ab6ed8d04d7bc7020d9b8e
xxxmkxxx <maks88648@gmail.com>
2022-01-30 19:25:34 +0400

    Создал интерфейс GameDAO

---
+++ src/main/java/com/xxxmkxxx/dao/GameDAO.java
@@ -0,0 +1,8 @@
+package com.xxxmkxxx.dao;
+
+import com.xxxmkxxx.models.GameModel;
+
+public interface GameDAO {
+    GameModel initializeComments(GameModel game);
+    GameModel initializeTeams(GameModel game);
+}
